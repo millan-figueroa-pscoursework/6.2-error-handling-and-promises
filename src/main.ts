d583dd907a6d7d13082c8e70d9fce216c584b7dd
millan-figueroa <millan.fig@gmail.com>
2025-10-22 00:16:53 -0700

    Refactor error handling: remove unused API practice file, implement custom error handling, and update main logic to process orders with validation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+import { handleOrder, processOrder } from './customErrorHandling';
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -8,3 +9,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 
+const order1 = { productId: "A1", quantity: 1, price: 20 };   // invalid quantity
+const order2 = { productId: "B2", quantity: 2, price: -5 };   // invalid price
+const order3 = { productId: "C3", quantity: 3, price: 100 };  // valid
+
+console.log(handleOrder(order1));
+console.log(handleOrder(order2));
+console.log(handleOrder(order3));
